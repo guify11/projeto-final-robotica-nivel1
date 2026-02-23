4a26f497d5ae9c61f8629808b480723520fa7dbb
Guilherme Silva Ferreira <guilherme.silva.ferreira@outlook.pt>
2026-02-23 20:34:08 +0000

    Add files via upload

---
+++ robo-arduino-tumo/robo-arduino-tumo.ino
@@ -0,0 +1,42 @@
+#define PIN_ENA A0
+#define PIN_IN1 16
+#define PIN_IN2 17
+#define PIN_IN3 18
+#define PIN_IN4 19
+#define PIN_ENB A5
+
+void setup() {
+  pinMode(PIN_ENA, OUTPUT);
+  pinMode(PIN_IN1, OUTPUT);
+  pinMode(PIN_IN2, OUTPUT);
+  pinMode(PIN_IN3, OUTPUT);
+  pinMode(PIN_IN4, OUTPUT);
+  pinMode(PIN_ENB, OUTPUT);
+}
+
+void andaTras() {
+  digitalWrite(PIN_IN1, HIGH);
+  digitalWrite(PIN_IN2, LOW);
+  analogWrite(PIN_ENA, 255);
+  digitalWrite(PIN_IN3, HIGH);
+  digitalWrite(PIN_IN4, LOW);
+  analogWrite(PIN_ENB, 255);
+
+}
+
+void andaFrente() {
+  digitalWrite(PIN_IN1, LOW);
+  digitalWrite(PIN_IN2, HIGH);
+  analogWrite(PIN_ENA, 255);
+  digitalWrite(PIN_IN3, LOW);
+  digitalWrite(PIN_IN4, HIGH);
+  analogWrite(PIN_ENB, 255);
+
+}
+
+void loop() {
+  andaTras();
+  delay(1000);
+  andaFrente();
+  delay(1000);
+}

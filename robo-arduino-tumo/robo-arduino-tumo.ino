683e9f9fd75f35589d70dd36c3e29dcd61f523a2
Guilherme Silva Ferreira <guilherme.silva.ferreira@outlook.pt>
2026-02-27 15:21:55 +0000

    tocar a musica do mario quando o robo encontrar a cor vermelha
    tocar a musica do mario quando o robo detectar que se encontra debaixo da cor vermelha

--- robo-arduino-tumo/robo-arduino-tumo.ino
+++ robo-arduino-tumo/robo-arduino-tumo.ino
@@ -8,24 +8,50 @@ MKRIoTCarrier carrier;
 #define PIN_IN4 19
 #define PIN_ENB A5
 
-uint32_t corbranca = carrier.leds.Color(255,255,255);
+uint32_t corbranca; // Definida no setup após o carrier.begin()
 
-
+// Notas Musicais para o Mario
+#define NOTE_E7  2637
+#define NOTE_C7  2093
+#define NOTE_G7  3136
+#define NOTE_G6  1568
 
 void setup() {
   Serial.begin(9600);
   delay(2000);
   carrier.noCase();
-  carrier.begin();
-  carrier.leds.fill(corbranca, 0 ,5);
+  if (!carrier.begin()) {
+    Serial.println("Erro ao iniciar o Carrier!");
+    while (1);
+  }
+  
+  corbranca = carrier.leds.Color(255, 255, 255);
+  carrier.leds.fill(corbranca, 0, 5);
   carrier.leds.setBrightness(255);
   carrier.leds.show();
+
   pinMode(PIN_ENA, OUTPUT);
   pinMode(PIN_IN1, OUTPUT);
   pinMode(PIN_IN2, OUTPUT);
   pinMode(PIN_IN3, OUTPUT);
   pinMode(PIN_IN4, OUTPUT);
   pinMode(PIN_ENB, OUTPUT);
+}
+
+// Função para tocar o tema curto do Mario
+void tocarMario() {
+  // Nota, Duração (ms)
+  int melodia[] = {NOTE_E7, NOTE_E7, 0, NOTE_E7, 0, NOTE_C7, NOTE_E7, 0, NOTE_G7, 0, NOTE_G6};
+  int duracoes[] = {150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150};
+
+  for (int i = 0; i < 11; i++) {
+    if (melodia[i] == 0) {
+      delay(duracoes[i]);
+    } else {
+      carrier.Buzzer.beep(melodia[i], duracoes[i]);
+      delay(duracoes[i] * 1.3); // Pequena pausa entre notas
+    }
+  }
 }
 
 void andaTras() {
@@ -35,7 +61,6 @@ void andaTras() {
   digitalWrite(PIN_IN3, HIGH);
   digitalWrite(PIN_IN4, LOW);
   analogWrite(PIN_ENB, 255);
-
 }
 
 void andaFrente() {
@@ -45,46 +70,46 @@ void andaFrente() {
   digitalWrite(PIN_IN3, LOW);
   digitalWrite(PIN_IN4, HIGH);
   analogWrite(PIN_ENB, 255);
-
 }
 
-void direita(){
+void direita() {
   digitalWrite(PIN_IN1, LOW);
   digitalWrite(PIN_IN2, HIGH);
   analogWrite(PIN_ENA, 180);
   analogWrite(PIN_ENB, 0);
 }
 
-void esquerda(){
+void esquerda() {
   analogWrite(PIN_ENA, 0);
   digitalWrite(PIN_IN3, LOW);
   digitalWrite(PIN_IN4, HIGH);
   analogWrite(PIN_ENB, 150);
 }
 
-void parar(){
+void parar() {
   analogWrite(PIN_ENA, 0);
   analogWrite(PIN_ENB, 0);
 }
 
 int r, g, b;
 
-void loop(){
+void loop() {
+  if (carrier.Light.colorAvailable()) {
+    carrier.Light.readColor(r, g, b);
+    Serial.println(String("r:") + r + String(" g:") + g + String(" b:") + b);
 
-    if (carrier.Light.colorAvailable()){
-     carrier.Light.readColor(r, g, b); //read rgb color values
-      Serial.println(String("r:")+r+String("g:")+g+String("b:")+b);
-      // check if color/light is bright enough
-      if (r >= 135 && g >= 135 && b >= 135){
-        Serial.println("estou no branco");
-        esquerda();
-      }
-      else if (r > 150 && r < 170 && g > 30 && g < 60 & b > 50 && b < 80) {
-        Serial.println("to in red");
-      }
-      else{
-        direita();
-        Serial.println("estou no preto");
-      }
+    if (r >= 135 && g >= 135 && b >= 135) {
+      Serial.println("estou no branco");
+      esquerda();
+    } 
+    else if (r > 150 && r < 170 && g > 30 && g < 60 && b > 50 && b < 80) {
+      Serial.println("TO NO VERMELHO - Toca Mario!");
+      parar();        // Para o robô para tocar a música
+      tocarMario();   // Chamei a função aqui!
+    } 
+    else {
+      direita();
+      Serial.println("estou no preto");
     }
   }
+}
